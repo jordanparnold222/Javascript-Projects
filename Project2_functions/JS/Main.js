35f12163c11ff03fd6b2b3c5738f33081c7fe2f8
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-05-25 18:46:31 -0700

    Quick Project2 submission fix

--- Project2_functions/JS/Main.js
+++ Project2_functions/JS/Main.js
@@ -1,10 +1,10 @@
-function first()    {
+function first()    {   //This function utilizes "GetElementByID" in its most basic form 
     var woah = "Woah, ";
     var IChanged = "I changed!";
     document.getElementById("paragraph").innerHTML = woah + IChanged;
 }
 
-function concatenation() {
+function concatenation() {  //This function utilizes "GetElementByID" in conjunction with the += operator
     var concatenated = "You don't see it, but this sentence is ";
     concatenated += "concatenated";
     document.getElementById("concatenate").innerHTML = concatenated;

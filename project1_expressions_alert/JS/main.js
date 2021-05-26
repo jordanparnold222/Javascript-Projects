6bac2f43e0e038d70bfa240d69238bfce9d711be
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-05-25 17:12:48 -0700

    1st javascript submission

---
+++ project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("This is an alert"); //utilizing the alert function, which causes a windows to pop up
+var family="Arnold", children="three", male="one", female="two";    //defining various variables
+var family = family.fontcolor("orange");    //giving a font color to a string variable
+var children = children.fontcolor("purple");    //giving a font color to a string variable
+var male = male.fontcolor("cyan");  //giving a font color to a string variable
+var female = female.fontcolor("brown"); //giving a font color to a string variable
+document.write(     //printing out the variables, concatenated
+    "Family name: " + family +
+    "Number of children: " + children + 
+    "Number of male children: " + male +
+    "Number of female children: " + female
+);
+document-write(5+7);    //printing the result of an expression

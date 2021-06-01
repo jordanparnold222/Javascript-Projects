ab240b88136b70aa9555e2396e5180ce023346a5
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-05-31 19:27:18 -0700

    Project7 submission

--- Project7_scope_time_function/JS/example.js
+++ Project7_scope_time_function/JS/example.js
@@ -1,3 +1,4 @@
+//Get Date example
 function GetDate()  {
     if (new Date().getHours() < 18) {
         document.getElementsById("greeting").innerHTML = "How are you doing today?";
@@ -5,4 +6,34 @@ function GetDate()  {
     else  {
         document.getElementById("greeting").innerHTML = "It's late. Go to bed."
     }
+}
+
+//Voting "else" example
+function GetDate()  {
+    var a = document.getElementById("age").value;
+
+    if (a < 18 )    {
+        document.getElementById("vote_output").innerHTML = "You are not old enough to vote. Obtain a fake ID and come back.";
+                    }
+        else {
+            document.getElementById("vote_output").innerHTML = "Your are old enough to vote!";
+        }
+}
+
+//time "else if" example
+
+
+function TimeOfDay()    {
+    var time = new Date().getHours();
+    var reply;
+    if (time < 12 == time > 0)  {
+        reply = "It is morning time";
+    }
+    else if (time >= 12 == time < 18)   {
+        reply = "It is afternoon";
+    }
+    else{
+        reply = "It is evening time";
+    }
+    document.getElementById("time_of_day").innerHTML = reply;
 }

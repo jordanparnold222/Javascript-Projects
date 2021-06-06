bbaff6e8245739f834a5e4977873434947dd57db
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-06-06 11:42:55 -0700

    Project9 submission

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -28,6 +28,11 @@ function plusSlides(n)  {
 }
 
 //THUMBNAIL IMAGE CONTROLS
+function currentSlide(n)    {
+    showSlides(slideIndex = n);
+}
+
+//THUMBNAIL IMAGE CONTROLS
 function showSlides(n)  {
 
     var i;
@@ -40,6 +45,10 @@ function showSlides(n)  {
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length;i++)    {
+        dots[i].className =dots[i].className.replace("active", "");
     }
     
     slides[slideIndex-1].style.display = "block";
